2589abb83a3e458b09a46c124bbca55468bf7ca6
blazej <dymekblazej@outlook.com>
2025-11-12 23:57:18 +0100

    Added a C# project based on FFmpeg.AutoGen

---
+++ StreamPlayerCore/Player.cs
@@ -0,0 +1,179 @@
+﻿using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+using SkiaSharp;
+
+namespace StreamPlayerCore;
+
+public enum RtspTransport
+{
+    Undefined = 0, 
+    Udp = 1, 
+    Tcp = 2, 
+    UdpMulticast = 3, 
+    Http = 4
+}
+
+public enum RtspFlags
+{
+    None = 0, 
+    FilterSrc = 1, 
+    Listen = 2, 
+    PreferTcp = 3
+}
+
+public delegate void FrameReady(SKBitmap frame);
+
+public class Player
+{
+    public event FrameReady? ProcessCompleted;
+    
+    private readonly CancellationTokenSource _tokenSource = new();
+    private bool _started;
+    private readonly unsafe AVDictionary* _optionsPtr;
+    private readonly AVHWDeviceType _hwDecodeDeviceType;
+    
+    public Player(string ffmpegRoot,
+        RtspTransport transport = RtspTransport.Undefined, RtspFlags flags = RtspFlags.None,
+        int analyzeDuration = 0, int probeSize = 4096, 
+        AVHWDeviceType hwDecodeDeviceType = AVHWDeviceType.AV_HWDEVICE_TYPE_NONE,
+        int ffmpegLogLevel = ffmpeg.AV_LOG_VERBOSE)
+    {
+        ffmpeg.RootPath = ffmpegRoot;
+        DynamicallyLoadedBindings.Initialize();
+        Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
+        unsafe
+        {
+            _optionsPtr = GetAvDict(transport, flags, analyzeDuration, probeSize);
+        }
+        _hwDecodeDeviceType = hwDecodeDeviceType;
+        SetupLogging(ffmpegLogLevel);
+    }
+
+    public unsafe void Start(Uri streamSource)
+    {
+        if (_started) return;
+        _started = true;
+        Task.Run(() =>
+        {
+            using var vsd = new VideoStreamDecoder(streamSource.AbsoluteUri, _hwDecodeDeviceType, _optionsPtr);
+            
+            Console.WriteLine($"codec name: {vsd.CodecName}");
+
+            var info = vsd.GetContextInfo();
+            info.ToList().ForEach(x => Console.WriteLine($"{x.Key} = {x.Value}"));
+
+            var sourceSize = vsd.FrameSize;
+            var sourcePixelFormat = _hwDecodeDeviceType == AVHWDeviceType.AV_HWDEVICE_TYPE_NONE
+                ? vsd.PixelFormat
+                : GetHwPixelFormat(_hwDecodeDeviceType);
+            var destinationSize = sourceSize;
+            var destinationPixelFormat = AVPixelFormat.AV_PIX_FMT_BGRA;
+            using var vfc = new VideoFrameConverter(sourceSize, sourcePixelFormat, destinationSize, destinationPixelFormat);
+
+            while (vsd.TryDecodeNextFrame(out var frame))
+            {
+                var convertedFrame = vfc.Convert(frame);
+                var imageInfo = new SKImageInfo(convertedFrame.width, convertedFrame.height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+                var bitmap = new SKBitmap();
+                bitmap.InstallPixels(imageInfo, (IntPtr)convertedFrame.data[0]);
+                OnProcessCompleted(bitmap);
+            }
+
+        }, _tokenSource.Token);
+    }
+
+    public void Stop()
+    {
+        if (!_started) return;
+        _tokenSource.Cancel();
+        _started = false;
+    }
+    
+    private static unsafe void SetupLogging(int ffmpegLogLevel)
+    {
+        ffmpeg.av_log_set_level(ffmpegLogLevel);
+        
+        // ReSharper disable once ConvertToLocalFunction
+        av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
+        {
+            if (level > ffmpeg.av_log_get_level()) return;
+
+            const int lineSize = 1024;
+            var lineBuffer = stackalloc byte[lineSize];
+            var printPrefix = 1;
+            ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
+            var line = Marshal.PtrToStringAnsi((IntPtr)lineBuffer);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(line);
+            Console.ResetColor();
+        };
+
+        ffmpeg.av_log_set_callback(logCallback);
+    }
+
+    private static unsafe AVDictionary* GetAvDict(RtspTransport transport, RtspFlags flags, int analyzeDuration, int probeSize)
+    {
+        AVDictionary* optionsPtr = null;
+        switch (transport)
+        {
+            case RtspTransport.Http:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_transport", "http", 0);
+                break;
+            case RtspTransport.Udp:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_transport", "udp", 0);
+                break;
+            case RtspTransport.Tcp:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_transport", "tcp", 0);
+                break;
+            case RtspTransport.UdpMulticast:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_transport", "udp_multicast", 0);
+                break;
+            case RtspTransport.Undefined:
+            default:
+                break;
+        }
+        switch (flags)
+        {
+            case RtspFlags.FilterSrc:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_flags", "filter_src", 0);
+                break;
+            case RtspFlags.Listen:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_flags", "listen", 0);
+                break;
+            case RtspFlags.PreferTcp:
+                ffmpeg.av_dict_set(&optionsPtr, "rtsp_flags", "prefer_tcp", 0);
+                break;
+            case RtspFlags.None:
+            default:
+                throw new ArgumentOutOfRangeException(nameof(flags), flags, null);
+        }
+        ffmpeg.av_dict_set(&optionsPtr, "analyzeduration", analyzeDuration.ToString(), 0);
+        ffmpeg.av_dict_set(&optionsPtr, "probesize", probeSize.ToString(), 0);
+
+        return optionsPtr;
+    }
+    
+    private static AVPixelFormat GetHwPixelFormat(AVHWDeviceType hWDevice)
+    {
+        return hWDevice switch
+        {
+            AVHWDeviceType.AV_HWDEVICE_TYPE_NONE => AVPixelFormat.AV_PIX_FMT_NONE,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_VDPAU => AVPixelFormat.AV_PIX_FMT_VDPAU,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_CUDA => AVPixelFormat.AV_PIX_FMT_CUDA,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_VAAPI => AVPixelFormat.AV_PIX_FMT_VAAPI,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_DXVA2 => AVPixelFormat.AV_PIX_FMT_NV12,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_QSV => AVPixelFormat.AV_PIX_FMT_QSV,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_VIDEOTOOLBOX => AVPixelFormat.AV_PIX_FMT_VIDEOTOOLBOX,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_D3D11VA => AVPixelFormat.AV_PIX_FMT_NV12,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_DRM => AVPixelFormat.AV_PIX_FMT_DRM_PRIME,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_OPENCL => AVPixelFormat.AV_PIX_FMT_OPENCL,
+            AVHWDeviceType.AV_HWDEVICE_TYPE_MEDIACODEC => AVPixelFormat.AV_PIX_FMT_MEDIACODEC,
+            _ => AVPixelFormat.AV_PIX_FMT_NONE
+        };
+    }
+
+    protected virtual void OnProcessCompleted(SKBitmap frame)
+    {
+        ProcessCompleted?.Invoke(frame);
+    }
+}
